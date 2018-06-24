2e124411e567c3044d9c0c7e9384097e9a40caa8
tzuhaochu <giroud.chu@gmail.com>
2018-06-24 13:15:01 +0800

    增加手写数字0-9识别的方法

--- ch2/kNN.py
+++ ch2/kNN.py
@@ -89,3 +89,28 @@ def img2vector(filename):
             returnVect[0, 32*i+j] = int(lineStr[j])
     return returnVect
 
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')            # 返回目录下文件list
+    m = len(trainingFileList)
+    trainingMat  = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]         # 去除后缀
+        classNumStr = int(fileStr.split('_')[0])            # 获得数字的图像标签
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("分类结果为： %d， 真实结果为： %d" % (classifierResult, classNumStr))
+        if classifierResult != classNumStr:
+            errorCount += 1.0
+    print("\n总错误数为： %d" % errorCount)
+    print("\n总错误率为： %f" % (errorCount/float(mTest)))
